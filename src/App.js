7a48308629a054e4e5992ef79f8d4f7aef724a67
nelsoneo <nelsoneo@gmail.com>
2021-06-30 23:20:40 -0300

    list

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ const App = ()=> {
    <>
     <h1>Hello World</h1>
     <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">Item 1</a>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">Item 2</a>
+        <a href="/" className="list-group-item list-group-item-action list-group-item-dark">Item 3</a>
     </ul>
    </>
   )
